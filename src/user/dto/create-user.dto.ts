fd03ff9e25b9131a366436b907366b9a4ae34ae9
husnainrazadevsinc <husnain.manzoor@devsinc.com>
2024-01-02 16:58:18 +0500

    UserModule: Applied Validations and configured UserService

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsUrl } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  age: number;
+
+  @IsNotEmpty()
+  gender: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  username: string;
+
+  @IsUrl()
+  @IsNotEmpty()
+  profilePicture: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schema/User';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    const model = new this.userModel();
+    model.name = createUserDto.name;
+    model.age = createUserDto.age;
+    model.gender = createUserDto.gender;
+    model.email = createUserDto.email;
+    model.username = createUserDto.username;
+    model.profilePicture = createUserDto.profilePicture;
+    return model.save();
   }
 
-  findAll() {
-    return `This action returns all user`;
+  findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string): Promise<User> {
+    return this.userModel.findById(id).exec();
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.userModel
+      .updateOne(
+        { _id: id },
+        {
+          name: updateUserDto.name,
+          age: updateUserDto.age,
+          gender: updateUserDto.gender,
+          email: updateUserDto.email,
+          username: updateUserDto.username,
+          profilePicture: updateUserDto.profilePicture,
+        },
+      )
+      .exec();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    return this.userModel.deleteOne({ _id: id }).exec();
   }
 }
